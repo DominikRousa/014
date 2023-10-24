6ef7306dda4b1f3c2329943fbfd3a8c8a72999de
DominikRousa <145441116+DominikRousa@users.noreply.github.com>
2023-10-24 11:57:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function calculate() {
+
+    const prvni = parseInt(document.getElementById("first").value)
+    const druhy = parseInt(document.getElementById("second").value)
+    const treti = parseInt(document.getElementById("third").value)
+
+    let result;
+
+    if (treti == ""){
+        result = Math.sqrt((prvni * prvni) + (druhy * druhy));
+    } else if (druhy == ""){
+        result = Math.sqrt((treti * treti) - (prvni * prvni));
+    } else if (prvni == ""){
+        result = Math.sqrt((treti * treti) - (druhy * druhy));
+    } else {
+        document.getElementById("final").innerHTML = "nejde"
+    }
+
+    console.log(prvni)
+    console.log(druhy)
+    console.log(treti)
+
+    document.getElementById("final").innerHTML = result
+}
